d70b27b4d4cb420146e2a7d87336448c9aa4fce2
phuduongdev <phu.duongtan@gmail.com>
2020-03-15 16:21:06 +0700

    update mongoose db url

--- src/config/mongooseDb.config.js
+++ src/config/mongooseDb.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/nodejs_crud_db'
+    url:
+        // 'mongodb+srv://admin:hqcogroup@2020@hqcogroup-g1ekt.mongodb.net/hqcompanydb'
+        'mongodb+srv://admin:hqcogroup@2020@hqcogroup-g1ekt.mongodb.net/hqcompanydb?retryWrites=true&w=majority'
 };
